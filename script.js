f177190743b8d515e7cfeb795bf19251c7c13e42
Akash Kotla <akashkotla1999@gmail.com>
2022-03-16 11:59:05 +0530

    source code

---
+++ script.js
@@ -0,0 +1,9 @@
+// let open = document.getElementById('open')
+// let close = document.getElementById('close')
+// let container = document.querySelectorAll('.container')
+
+// open.addEventListener('click', () => container.classList.add('show-nav'));
+// close.addEventListener('click', () => container.classList.remove('show-nav'));
+
+
+// o
